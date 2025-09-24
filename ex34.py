8303f8ee3cd9071fc4c73556625f58580e6309aa
carloslevy117-ctrl <carloslevy117@gmail.com>
2025-09-24 17:26:33 -0300

    finalizado

--- ex34.py
+++ ex34.py
@@ -6,5 +6,17 @@
 #begin_inputs
 # programa sem entrada (input)
 #end_inputs
+dia = 1
+mes = 1
+ano = 2017
+while ano <= 2020:
+    mes = 1
+    while mes <= 12:
+        dia = 1
+        while dia <=  30:
+            print(f'{dia}/{mes}/{ano}')
+            dia += 1
+        mes += 1
+    ano += 1
 
 	
