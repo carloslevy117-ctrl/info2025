6c6436acfbefb9987156c1b04defd6a1fe1a3914
carloslevy117-ctrl <carloslevy117@gmail.com>
2025-09-30 13:22:38 -0300

    fiz o 4.1

---
+++ ex41.py
@@ -0,0 +1,15 @@
+# @cikey 9938fefe4aecf4a9a53709cde00894a5
+# @sid 20251174010018
+# @aid V4.1
+
+
+#begin_inputs
+x = int(input("digite o numero:"))
+y = int(input("digite um numero:"))
+#end_inputs
+if x>y:
+    print(x)
+else:
+    print(y)
+
+	
