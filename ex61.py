dfff7a3dd33803d38bf359e132e60f4fbc3ee591
carloslevy117-ctrl <carloslevy117@gmail.com>
2025-10-09 23:20:09 -0300

    Create ex61.py

---
+++ ex61.py
@@ -0,0 +1,11 @@
+1. # @cikey 9938fefe4aecf4a9a53709cde00894a5
+ 2. # @sid 20251174010018
+ 3. # @aid V6.1
+ 4. 
+ 5. 
+ 6. #begin_inputs
+ 7. 
+ 8. #end_inputs
+ 9. import random
+10. for n in range(6):
+11.     print(random.randint(1,51))
