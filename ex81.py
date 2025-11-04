f92bad6ce10032e22bd192725a8d62d9e0891c68
carloslevy117-ctrl <carloslevy117@gmail.com>
2025-11-04 14:27:20 -0300

    Create ex81.py

---
+++ ex81.py
@@ -0,0 +1,14 @@
+# @cikey 9938fefe4aecf4a9a53709cde00894a5
+# @sid 20251174010018
+# @aid V8.1
+
+def escrever():
+    frase = input("digite uma frase:")
+
+def lerfrase(frase):
+    print("frase:", frase)
+
+escrever()
+lerfrase("frase")
+
+
